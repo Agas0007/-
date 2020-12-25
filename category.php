b217a823fe387686da552205503db0ba543ff7ad
Agas0007 <BabarykaSES@yandex.ru>
2020-12-25 18:18:54 +0300

    рабочий код
    Вывод категорий Woocomerce на главную страницу

--- category.php
+++ category.php
@@ -9,8 +9,12 @@
       foreach ($categories as $parent_category): ?>     
         <a href="<?= get_term_link($parent_category->term_id, 'product_cat') ?>" class="category_article">
           <div class="category_article_img">
-            <img src="<?= get_thumbnail_url($parent_category->term_id, 'product_cat') ?>" alt="<?= $parent_category->name ?>">
+              <?
+                $thumbnail_id = get_woocommerce_term_meta( $parent_category->term_id, 'thumbnail_id', true );
+                $image = wp_get_attachment_url( $thumbnail_id );
+              ?>
+            <img src="<?= $image ?>" alt="<?= $parent_category->name ?>">
           </div>
           <h3><?= $parent_category->name ?></h3>
         </a>
-      <? endforeach ?>  
+      <? endforeach ?>
